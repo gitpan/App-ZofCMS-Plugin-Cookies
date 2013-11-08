74249639c98dfdef343138cd9ed7c09dbb3f653e
Zoffix Znet <zoffixcpan@gmail.com>
2013-11-08 03:31:45 +0000

    Import of ZOFFIX/App-ZofCMS-Plugin-Cookies-0.0105 from CPAN.
    gitpan-cpan-distribution: App-ZofCMS-Plugin-Cookies
    gitpan-cpan-version:      0.0105
    gitpan-cpan-path:         ZOFFIX/App-ZofCMS-Plugin-Cookies-0.0105.tar.gz
    gitpan-cpan-author:       ZOFFIX
    gitpan-cpan-maturity:     released

--- lib/App/ZofCMS/Plugin/Cookies.pm
+++ lib/App/ZofCMS/Plugin/Cookies.pm
@@ -3,7 +3,7 @@ package App::ZofCMS::Plugin::Cookies;
 use warnings;
 use strict;
 
-our $VERSION = '0.0104';
+our $VERSION = '0.0105';
 
 sub new { return bless {}, shift }
 
@@ -38,6 +38,8 @@ sub process {
 
 1;
 __END__
+
+=encoding utf8
 
 =head1 NAME
 
